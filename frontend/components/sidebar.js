7e8ae46b4368603198242912174caf04a4a61089
RafaelaTGL <rafatrageluc@gmail.com>
2026-05-20 17:05:15 -0300

    nova logo

--- frontend/components/sidebar.js
+++ frontend/components/sidebar.js
@@ -1,18 +1,50 @@
 window.ConectaComponents = window.ConectaComponents || {};
-window.ConectaComponents.sidebar = `<aside class="sidebar">
-      <a class="brand" href="../index.html">
-        <span class="brand-mark"><span class="iconify" data-icon="ph:rocket-launch-bold"></span></span>
-        <span>DevStart</span>
-      </a>
 
-      <nav class="side-nav">
-        <a class="active" href="dashboard.html"><span class="iconify" data-icon="lucide:home"></span>Início</a>
-        <a href="cursos.html"><span class="iconify" data-icon="lucide:graduation-cap"></span>Cursos</a>
-        <a href="areas-ti.html"><span class="iconify" data-icon="lucide:code-xml"></span>Áreas da TI</a>
-        <a href="curriculo.html"><span class="iconify" data-icon="lucide:file-user"></span>Currículo</a>
-        <a href="oportunidades.html"><span class="iconify"
-            data-icon="lucide:briefcase-business"></span>Oportunidades</a>
-        <a href="ferramentas.html"><span class="iconify" data-icon="lucide:wrench"></span>Ferramentas</a>
-        <a href="roadmap.html"><span class="iconify" data-icon="lucide:folder-code"></span>Projetos</a>
-      </nav>
-    </aside>`;
+window.ConectaComponents.sidebar = `
+<aside class="sidebar">
+  <a class="brand" href="../index.html">
+    <span class="brand-mark">
+      <img src="../assets/LogoDS.svg" alt="DevStart Logo">
+    </span>
+
+    <span>DevStart</span>
+  </a>
+
+  <nav class="side-nav">
+    <a class="active" href="dashboard.html">
+      <span class="iconify" data-icon="lucide:home"></span>
+      Início
+    </a>
+
+    <a href="cursos.html">
+      <span class="iconify" data-icon="lucide:graduation-cap"></span>
+      Cursos
+    </a>
+
+    <a href="areas-ti.html">
+      <span class="iconify" data-icon="lucide:code-xml"></span>
+      Áreas da TI
+    </a>
+
+    <a href="curriculo.html">
+      <span class="iconify" data-icon="lucide:file-user"></span>
+      Currículo
+    </a>
+
+    <a href="oportunidades.html">
+      <span class="iconify" data-icon="lucide:briefcase-business"></span>
+      Oportunidades
+    </a>
+
+    <a href="ferramentas.html">
+      <span class="iconify" data-icon="lucide:wrench"></span>
+      Ferramentas
+    </a>
+
+    <a href="roadmap.html">
+      <span class="iconify" data-icon="lucide:folder-code"></span>
+      Projetos
+    </a>
+  </nav>
+</aside>
+`;
